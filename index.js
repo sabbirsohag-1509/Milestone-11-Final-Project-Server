ab21643ed91aa96af6217ed4e853bcfcd691a895
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-11-20 23:46:58 +0600

    delete api

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
-
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.eyaxb6h.mongodb.net/?appName=myCluster`;
 
@@ -19,34 +17,45 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     await client.connect();
-    
-      const myDB = client.db('zap_shift_db');
-      const parcelCollections = myDB.collection("parcels");
-      
-      //parcel api
-      //post
-      app.post("/parcels", async (req, res) => {
-          const parcel = req.body;
-          const result = await parcelCollections.insertOne(parcel);
-          res.send(result);
-      });
-      //get
-      app.get("/parcels", async (req, res) => {
-          const email = req.query.email;
-          const result = await parcelCollections.find({senderEmail: email}).toArray();
-          res.send(result);
-      })
-      
-      
+
+    const myDB = client.db("zap_shift_db");
+    const parcelCollections = myDB.collection("parcels");
+
+    //parcel api
+    //post
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      //parcel created time
+      parcel.createdAt = new Date();
+      const result = await parcelCollections.insertOne(parcel);
+      res.send(result);
+    });
+    //get
+    app.get("/parcels", async (req, res) => {
+      const email = req.query.email;
+      const result = await parcelCollections
+        .find({ senderEmail: email }).sort({ createdAt: -1 })
+        .toArray();
+      res.send(result);
+    });
+    //delete
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelCollections.deleteOne(query);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,10 +63,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('zap-shift server is running!');
-})
+app.get("/", (req, res) => {
+  res.send("zap-shift server is running!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
